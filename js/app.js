4fc5797c89dba977931d875707ade2d7a6a7e516
scruffy21 <yougotmebeggingyouformercy@gmail.com>
2018-06-11 10:55:15 +0200

    modal

--- js/app.js
+++ js/app.js
@@ -6,6 +6,8 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = 0;
+    this.y = 50;
 };
 
 // Update the enemy's position, required method for game
@@ -25,16 +27,51 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+var Player = function (character) {
+    this.sprite = `images/char-${character}.png`;
+    this.speedX = 101;
+    this.speedY = 83;
+    this.x = 200;
+    this.y = 380;
+};
+
+Player.prototype.update = function (dt) {
+    // *
+}
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+Player.prototype.handleInput = function (key) {
+    switch (key) {
+        case "left":
+            this.x -= this.speedX;
+            break;
+        case "up":
+            this.y -= this.speedY;
+            break;
+        case "right":
+            this.x += this.speedX;
+            break;
+        case "down":
+            this.y += this.speedY;
+            break;    
+    }
+}
+
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+const allEnemies = [new Enemy()];
 
+const player = new Player("boy");
 
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keydown', function(e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
